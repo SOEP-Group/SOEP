15ba8dc0593adbfba74ca564d670edcbdeb0e889
Ivan Sedelkin <ivan.sedelkin@rocksigma.com>
2024-09-23 18:01:04 +0200

    Added verify

--- src/core/assert.h
+++ src/core/assert.h
@@ -15,7 +15,7 @@
 #define SOEP_ENABLE_ASSERTS
 #endif
 
-
+// Use to track bugs during debug. Most stuff should be asserted instead of verified
 #ifdef SOEP_ENABLE_ASSERTS
 #ifdef PLATFORM_LINUX
 #define SOEP_ASSERT_MESSAGE_INTERNAL(...)  spdlog::error( "Assertion Failed", ##__VA_ARGS__)
@@ -26,3 +26,12 @@
 #else
 #define SOEP_ASSERT(condition, ...)
 #endif
+
+
+// These work similairly to asserts but are still there during release. Use these sparingly on things that you cannot gurantee is fixed
+#ifdef PLATFORM_LINUX
+#define SOEP_VERIFY_MESSAGE_INTERNAL(...)  spdlog::error( "Verification Failed", ##__VA_ARGS__)
+#else
+#define SOEP_VERIFY_MESSAGE_INTERNAL(...)  spdlog::error("Verification Failed" __VA_OPT__(,) __VA_ARGS__)
+#endif
+#define SOEP_VERIFY(condition, ...) { if(!(condition)) { SOEP_VERIFY_MESSAGE_INTERNAL(__VA_ARGS__); SOEP_DEBUG_BREAK; } }
